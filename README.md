115f13f5fcd30af93e99814dd9ca11aa0aa90201
Danh Doan <danhdoan.es@gmail.com>
2019-08-08 10:58:31 +0700

    Add ResNet models

--- README.md
+++ README.md
@@ -10,17 +10,29 @@ This repository serves my purpose of implementing and experiencing different mod
 ## Current Results
 |Architecture | Model | Accuracy|
 |--|--|--|
-|VGG| VGG11| 95.68%|
-||VGG13|95.06%|
-||VGG16|94.32%|
-||VGG19|90.66%|
+| VGG | VGG11 | 95.68% |
+|| VGG13 | 95.06% |
+|| VGG16 | 94.32% |
+|| VGG19 | 90.66% |
+|ResNet | ResNet18 | 91.12% |
+|| ResNet34 | 99.35% |
+|| ResNet50 | 98.66% |
+|| ResNet101 | 99.11% |
+|| ResNet152 | 99.35% |
 
 ## Training History
 * VGG11:
 
 ![VGG11](history/vgg11-95.68.png)
 
+* ResNet152:
+
+![ResNet152](history/resnet152-99.35.png)
+
 ## Latest Updates
+* 2019, Aug 8:
+  * Implement and Test with all **ResNet** architectures [[paper]](https://arxiv.org/abs/1512.03385)
+
 * 2019, Aug 7:
 	* Set up the training and test program
 	* Implement and Test with all **VGG** architectures [[paper]](https://arxiv.org/abs/1409.1556)
---
+++ altusi/models/resnet.py
@@ -0,0 +1,125 @@
+"""
+ResNet Network
+==============
+"""
+
+
+from mxnet import nd
+from mxnet.gluon import nn
+
+
+class BasicBlock(nn.HybridBlock):
+    def __init__(self, nchannels, downsample=False, strides=1, **kwargs):
+        super(BasicBlock, self).__init__(**kwargs)
+        
+        self.body = nn.HybridSequential()
+        self.body.add(nn.Conv2D(nchannels, kernel_size=3, strides=strides, padding=1))
+        self.body.add(nn.BatchNorm())
+        self.body.add(nn.Activation('relu'))
+        
+        self.body.add(nn.Conv2D(nchannels, kernel_size=3, padding=1))
+        self.body.add(nn.BatchNorm())
+        
+        if downsample:
+            self.shortcut = nn.Conv2D(nchannels, kernel_size=1, strides=strides)
+        else:
+            self.shortcut = None
+            
+            
+    def hybrid_forward(self, F, X):
+        y = self.body(X)
+        
+        if self.shortcut:
+            X = self.shortcut(X)
+            
+        return F.Activation(y + X, act_type='relu')
+
+
+class BottleNeck(nn.HybridBlock):
+    def __init__(self, nchannels, downsample=False, strides=1, **kwargs):
+        super(BottleNeck, self).__init__(**kwargs)
+        
+        self.body = nn.HybridSequential()
+        self.body.add(nn.Conv2D(nchannels//4, kernel_size=1, strides=strides))
+        self.body.add(nn.BatchNorm())
+        self.body.add(nn.Activation('relu'))
+        
+        self.body.add(nn.Conv2D(nchannels//4, kernel_size=3, strides=1, padding=1))
+        self.body.add(nn.BatchNorm())
+        self.body.add(nn.Activation('relu'))
+        
+        self.body.add(nn.Conv2D(nchannels, kernel_size=1))
+        self.body.add(nn.BatchNorm())
+        
+        if downsample:
+            self.shortcut = nn.Conv2D(nchannels, kernel_size=1, strides=strides)
+        else:
+            self.shortcut = None
+            
+    
+    def hybrid_forward(self, F, X):
+        y = self.body(X)
+        
+        if self.shortcut:
+            X = self.shortcut(X)
+            
+        return F.Activation(y + X, act_type='relu')
+
+
+def _make_layer(block, nblocks, nchannels, strides, in_channels):
+    layer = nn.HybridSequential()
+    
+    layer.add(block(nchannels, nchannels != in_channels, strides))
+    for _ in range(nblocks-1):
+        layer.add(block(nchannels, False))
+        
+    return layer
+
+
+def get_resnet(block, layers, channels, nclasses):
+    net = nn.HybridSequential()
+
+    net.add(nn.Conv2D(16, kernel_size=3, strides=1, padding=1),
+            nn.BatchNorm(),
+            nn.Activation('relu'),
+            nn.MaxPool2D(pool_size=3, strides=2, padding=1))
+
+    for i, nblocks in enumerate(layers):
+        strides = 1 if i == 0 else 2
+        net.add(_make_layer(block, nblocks, channels[i+1], strides, channels[i]))
+
+    net.add(nn.BatchNorm(),
+            nn.Activation('relu'),
+            nn.GlobalAvgPool2D(),
+            nn.Dense(nclasses))
+
+    return net
+
+
+resnet_archs = {
+    18:  [BasicBlock, [2, 2, 2, 2],  [16, 16, 32, 64, 128]],
+    34:  [BasicBlock, [3, 4, 6, 3],  [16, 16, 32, 64, 128]],
+    50:  [BottleNeck, [3, 4, 6, 3],  [16, 64, 128, 256, 512]],
+    101: [BottleNeck, [3, 4, 23, 3], [16, 64, 128, 256, 512]],
+    152: [BottleNeck, [3, 8, 36, 3], [16, 64, 128, 256, 512]]
+}
+
+
+def ResNet18(nclasses):
+    return get_resnet(*resnet_archs[18], nclasses)
+
+
+def ResNet34(nclasses):
+    return get_resnet(*resnet_archs[34], nclasses)
+
+
+def ResNet50(nclasses):
+    return get_resnet(*resnet_archs[50], nclasses)
+
+
+def ResNet101(nclasses):
+    return get_resnet(*resnet_archs[101], nclasses)
+
+
+def ResNet152(nclasses):
+    return get_resnet(*resnet_archs[152], nclasses)
